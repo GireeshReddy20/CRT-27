d80c311b9416e31fa6fcb76fdda8c3e7c4378d14
GireeshReddy20 <gireeshreddy0807@gmail.com>
2025-08-19 15:34:18 +0530

    Add files via upload

--- sample.java
+++ sample.java
@@ -1,29 +1,46 @@
-public class sample{
-
-        int id;
-        String name;
-        int age;
-        String branch;
-        long year;
-
-    void setdetails(int id,String name,int age,String branch,long year){
-        this.id=id;
-        this.name=name;
-        this.age=age;
-        this.branch=branch;
-        this.year=year;
+public class sample {
+        int studentId;
+        String studentName;
+        int studentAge;
+        String studentBranch;
+        long studentYear;
+        int teacherId;
+        String teacherName;
+        String subject;
+        void setStudentDetails(int studentId, String studentName, int studentAge, String studentBranch, long studentYear) {
+            this.studentId = studentId;
+            this.studentName =studentName;
+            this.studentAge = studentAge;
+            this.studentBranch = studentBranch;
+            this.studentYear = studentYear;
+        }
+        void setTeacherDetails(int teacherId, String teacherName, String subject) {
+            this.teacherId = teacherId;
+            this.teacherName = teacherName;
+            this.subject = subject;
+        }
+        void getStudentDetails() {
+            System.out.println("Student Details:");
+            System.out.println("Student ID: " +studentId);
+            System.out.println("Student Name: " +studentName);
+            System.out.println("Student Age: " +studentAge);
+            System.out.println("Student Branch: " +studentBranch);
+            System.out.println("Student Year: " +studentYear);
+        }
+        void getTeacherDetails() {
+            System.out.println("Teacher Details:");
+            System.out.println("Teacher ID: " + teacherId);
+            System.out.println("Teacher Name: " + teacherName);
+            System.out.println("Subject: " + subject);
+        }
+        public static void main(String[] args) {
+            sample s = new sample();
+            s.setStudentDetails(237013269, "Ram", 20, "AI&DS", 2027);
+            s.setTeacherDetails(23756, "Dr.Ashok", "Artificial Intelligence");
+            s.getStudentDetails();
+            System.out.println();
+            s.getTeacherDetails();
+        }
     }
-    void getdetails(){
-        System.out.println("id:"+id);
-        System.out.println("name:"+name);
-        System.out.println("age:"+age);
-        System.out.println("branch:"+branch);
-        System.out.println("year:"+year);
 
-    }
-    public static void main(String[] args) {
-        sample s=new sample();
-        s.setdetails(1,"Abishek",20,"AI&DS",2023);
-        s.getdetails();
-    }
-}
+
